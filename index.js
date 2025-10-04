9c069bcac18a67ece0a4e1f9f5dafb9962537c70
hajhouj <alaa.spread@gmail.com>
2025-10-04 22:05:15 +0100

    changement dans le fichier index.js

--- index.js
+++ index.js
@@ -7,20 +7,51 @@ app.use(cors());
 
 
 
-app.get("/",(req,res)=>{
-const sql = 'insert into Admine (nom,prenom,image,motdepasse,email) values ("hajhouj","alaa","image1","gdgdfdf","alaa.spread@gmail.com")';
-const sql2="delete from Admine";
+app.get("/:nom/:prenom",(req,res)=>{
+const nom1=req.params.nom;
+const prenom=req.params.prenom;
+const sql = `insert into Admine (nom,prenom,image,motdepasse,email) values ("${nom1}","${prenom}","image1","gdgdfdf","alaa.spread@gmail.com")`;
 
-  conn.query(sql, (err, results) => {
-    if (err) {
-      console.error('Erreur lors de l’exécution de la requête :');
-      return res.status(500).send('Erreur serveur');
-    }
-    console.log("requet passsée avec succes!!!!")
-    res.send(results)
-  });
+      conn.query(sql, (err, results) => {
+              if (err) {
+                console.error('Erreur lors de l’exécution de la requête :');
+                return res.status(500).send('Erreur serveur');
+              }
 
 
+              console.log("requet passsée avec succes!!!!");
+              res.send('requete passée avec succes');
+            });
+
+
+})
+
+
+
+
+app.get("/supprimer",(req,res)=>{
+console.log("debut de la demande ");
+const sql='delete from Admine';
+conn.query(sql,(err,results)=>{
+  if (err) {
+    console.error('Erreur lors de l’exécution de la requête :');
+    return res.status(500).send('Erreur serveur');
+  }
+
+  
+  console.log("requet passsée avec succes!!!!");
+  res.send('suppression avec succes');
+
+})
+})
+
+
+
+
+app.get("/tester/:id",(req,res)=>{
+console.log(req.params);
+console.log('demande pour l\'endpoint tester');
+res.send("demande traitée");
 })
 
 
